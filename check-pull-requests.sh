cb465b08587798aa788dfd9bc345c2c982ac9e29
Josh Soref <jsoref@users.noreply.github.com>
2020-02-19 13:43:33 -0500

    Implement a working approach to retrieving foreign PRs

--- check-pull-requests.sh
+++ check-pull-requests.sh
@@ -31,8 +31,10 @@ fi
 cat "$pulls" | jq -c '.[]'|jq -c -r '{
  head_repo : .head.repo.full_name,
  base_repo: .base.repo.full_name,
+ head_ref: .head.ref,
  head_sha: .head.sha,
  base_sha: .base.sha,
+ clone_url: .head.repo.clone_url,
  merge_commit_sha: .merge_commit_sha,
  updated_at: .updated_at,
  commits_url: .commits_url,
@@ -56,6 +58,9 @@ for a in $(cat "$escaped"); do
   merge_commit_sha=$(cat $pull | jq -r .merge_commit_sha)
   comments_url=$(cat $pull | jq -r .comments_url)
   commits_url=$(cat $pull | jq -r .commits_url)
+  clone_url=$(cat $pull | jq -r .clone_url)
+  clone_url=$(echo "$clone_url" | sed -e 's/https/http/')
+  head_ref=$(cat $pull | jq -r .head_ref)
   echo "do work for $head_repo -> $base_repo: $head_sha as $merge_commit_sha"
   export GITHUB_SHA="$head_sha"
   export GITHUB_EVENT_NAME=pull_request
@@ -67,7 +72,11 @@ for a in $(cat "$escaped"); do
     -r '{pull_request: {base: {sha: $base_sha}, head: {sha: $head_sha}, comments_url: $comments_url, commits_url: $commits_url }}' \
     > "$fake_event"
   export GITHUB_EVENT_PATH="$fake_event"
-  git fetch origin $merge_commit_sha
-  git checkout $merge_commit_sha
+  git remote rm pr 2>/dev/null || true
+  git remote add pr $clone_url
+  cat .git/config
+  git fetch pr $head_ref
+  git checkout $head_sha
+  git remote rm pr 2>/dev/null || true
   "$spellchecker/unknown-words.sh" || true
 done
