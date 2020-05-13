d6713c1afa30083f599daa1bc04cafff8d08c7cb
Josh Soref <jsoref@users.noreply.github.com>
2020-05-12 21:16:14 -0400

    common: add include guard

--- common.sh
+++ common.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-if [ -n "$DEBUG" ]; then
-  set -x
-  begin_group() {
-    echo "::group::$1"
+if [ "$DEBUG" != defined ]; then
+  now() {
+    date +'%s%N'
   }
-  end_group() {
-    echo '::end_group::'
-  }
-else
-  begin_group() {
-    echo "(...$1...)"
-  }
-  end_group() {
-    :
-  }
+  start=$(now)
+  temp="${temp:-/tmp/spelling}"
+  export temp
+  if [ -n "$DEBUG" ]; then
+    set -x
+    begin_group() {
+      echo "::group::$1"
+    }
+    end_group() {
+      echo '::end_group::'
+    }
+  else
+    begin_group() {
+      echo "(...$1...)"
+    }
+    end_group() {
+      :
+    }
+    DEBUG=defined
+  fi
 fi
-
-now() {
-  date +'%s%N'
-}
-start=$(now)
-temp="${temp:-/tmp/spelling}"
-export temp
