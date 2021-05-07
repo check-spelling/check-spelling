1753c05dd665abdbf235d2ff4caef0e7e03137d3
Josh Soref <jsoref@users.noreply.github.com>
2021-05-07 08:13:19 -0400

    Show the payload instead of the transient path to it...

--- unknown-words.sh
+++ unknown-words.sh
@@ -104,7 +104,7 @@ react_comment_and_die() {
     if [ $res -gt 0 ]; then
       if [ -z "$DEBUG" ]; then
         echo "failed posting to $COMMENTS_URL"
-        echo "$PAYLOAD"
+        cat "$PAYLOAD"
       fi
       return $res
     fi
@@ -967,7 +967,7 @@ post_commit_comment() {
         if [ $res -gt 0 ]; then
           if [ -z "$DEBUG" ]; then
             echo "failed posting to $COMMENTS_URL"
-            echo "$PAYLOAD"
+            cat "$PAYLOAD"
           fi
           no_patch=1
         fi
