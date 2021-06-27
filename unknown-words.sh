7f9e612e9836712afadc17e4943045e6d7a9aadd
Josh Soref <jsoref@users.noreply.github.com>
2021-06-27 03:15:53 -0400

    Restrict where configuration files can live
    * Files must live in the workspace
    * Files can't live in git configuration directories

--- unknown-words.sh
+++ unknown-words.sh
@@ -88,7 +88,28 @@ check_for_newline_at_eof() {
 }
 
 cleanup_file() {
-  maybe_bad="$1"
+  export maybe_bad="$1"
+
+  result=0
+  perl -e '
+    use Cwd qw(abs_path);
+    my $maybe_bad=abs_path($ENV{maybe_bad});
+    my $workspace_path=abs_path($ENV{GITHUB_WORKSPACE});
+    if ($maybe_bad !~ /^\Q$workspace_path\E/) {
+      print "::error ::Configuration files must live within $workspace_path...\n";
+      print "::error ::Unfortunately, file $maybe_bad appears to reside elsewhere.\n";
+      exit 3;
+    }
+    if ($maybe_bad =~ m{/\.git/}i) {
+      print "::error ::Configuration files must not live within `.git/`...\n";
+      print "::error ::Unfortunately, file $maybe_bad appears to.\n";
+      exit 4;
+    }
+  ' || result=$?
+  if [ $result -gt 0 ]; then
+    quit $result
+  fi
+
   type="$2"
   case "$type" in
     patterns|excludes|only)
