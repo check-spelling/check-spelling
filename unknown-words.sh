80fbd3fcd0bafc24312e64b8a8793ed86375731d
Josh Soref <jsoref@users.noreply.github.com>
2021-07-13 23:17:38 -0400

    Refactor fewer_misspellings guard

--- unknown-words.sh
+++ unknown-words.sh
@@ -1388,10 +1388,6 @@ make_instructions() {
 }
 
 fewer_misspellings() {
-  if [ -n "$patch_add" ]; then
-    return
-  fi
-
   begin_group 'Fewer misspellings'
   title='There are now fewer misspellings than before'
   SKIP_CURL=1
@@ -1478,6 +1474,8 @@ if [ -n "$INPUT_REPORT_TITLE_SUFFIX" ]; then
 fi
 fewer_misspellings_canary=$(mktemp)
 set_patch_remove_add
-fewer_misspellings
+if [ -z "$patch_add" ]; then
+  fewer_misspellings
+fi
 more_misspellings
 cat $output_variables
