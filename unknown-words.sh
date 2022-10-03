161adefa652d02c57393170ae93d5bb09e09212d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-02 21:59:17 -0400

    Use GITHUB_ACTION_REPOSITORY for bug ref

--- unknown-words.sh
+++ unknown-words.sh
@@ -1643,8 +1643,8 @@ spelling_body() {
     push)
       details_note="See the [:scroll:action log]($(get_action_log)) for details.";;
     *)
-      details_note=$(echo '<!-- If you can see this, please [file a bug](https://github.com/check-spelling/check-spelling/issues/new)
-        referencing this comment url, as the code does not expect this to happen. -->' | strip_lead);;
+      details_note=$(echo "<!-- If you can see this, please [file a bug](https://github.com/$GITHUB_ACTION_REPOSITORY/issues/new)
+        referencing this comment url, as the code does not expect this to happen. -->" | strip_lead);;
   esac
   if [ -z "$err" ] && [ -e "$fewer_misspellings_canary" ]; then
     output_remove_items="$N$(remove_items)"
