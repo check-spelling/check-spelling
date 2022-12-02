09fcf220a1b52f659f927541dbe4393bc7dc5e58
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-01 22:50:44 -0500

    Add icon for automatically ignored section

--- unknown-words.sh
+++ unknown-words.sh
@@ -2128,7 +2128,7 @@ spelling_body() {
       calculate_exclude_patterns
       echo "skipped_files=$should_exclude_file" >> "$output_variables"
       output_excludes="$(echo "
-        <details><summary>Some files were automatically ignored</summary>
+        <details><summary>Some files were automatically ignored :see_no_evil:</summary>
 
         These sample patterns would exclude them:
         $B
@@ -2411,7 +2411,7 @@ minimize_comment_body() {
   if [ "$payload_size" -le "$github_comment_size_limit" ]; then
     return 0
   fi
-  trim_commit_comment 'Files' '(<details><summary>Some files were automatically ignored</summary>)\n.*?\`\`\`(.*?)\`\`\`.*?(?=</details>)' '\n\n'
+  trim_commit_comment 'Files' '(<details><summary>Some files were automatically ignored.*</summary>)\n.*?\`\`\`(.*?)\`\`\`.*?(?=</details>)' '\n\n'
   if [ "$payload_size" -le "$github_comment_size_limit" ]; then
     return 0
   fi
