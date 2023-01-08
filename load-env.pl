11613d0d2756c30231c704cfcb52b46d18b62117
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-07 22:45:22 -0500

    Handle single quotes in values

--- load-env.pl
+++ load-env.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use JSON::PP;
 my $input = $ENV{INPUTS};
+exit unless $input;
 my %inputs = %{decode_json $input};
 for my $key (keys %inputs) {
     next unless $key;
@@ -8,7 +9,8 @@ for my $key (keys %inputs) {
     next unless $val;
     my $var = $key;
     next if $var =~ /[-\s]/;
-    $val =~ s/([\$'])/\\$1/g;
+    $val =~ s/([\$])/\\$1/g;
+    $val =~ s/'/'"'"'/g;
     $var = uc $var;
     print qq<export INPUT_$var='$val';\n>;
 }
