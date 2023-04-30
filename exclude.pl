2f65afe9ace1eed6aafa5c1a1d2f2dd2171c885f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-30 01:03:03 -0400

    exclude: Use env vars to pass locations

--- exclude.pl
+++ exclude.pl
@@ -1,12 +1,6 @@
 #!/usr/bin/env -S perl -T
 
 use warnings;
-use File::Basename;
 use CheckSpelling::Exclude;
 
-my $dirname = dirname(__FILE__);
-my $exclude_file = $dirname.'/excludes.txt';
-my $only_file = $dirname.'/only.txt';
-$ENV{'exclude_file'} = $exclude_file if -e $exclude_file;
-$ENV{'only_file'} = $only_file if -e $only_file;
 CheckSpelling::Exclude::main();
