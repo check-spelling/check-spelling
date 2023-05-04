e43490d87c6733d9f171165d9226e6c9f730d8be
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-05-04 19:08:51 -0400

    Refactor CheckSpelling::Yaml

--- check-yaml-key-value.pl
+++ check-yaml-key-value.pl
@@ -1,44 +1,14 @@
 #!/usr/bin/env perl
-my ($key, $value) = (quotemeta($ENV{KEY}), quotemeta($ENV{VALUE}));
-my ($state, $gh_yaml_mode) = (0, '');
-my @nests;
-my ($start_line, $start_pod, $end);
-sub report {
-    print "$ARGV:$start_line:$start_pos ... $end, $ENV{MESSAGE}\n";
-    exit;
-}
-while (<>) {
-    if (/^(\s*)#/) {
-        $end += length $_ if ($state == 3);
-        next;
-    }
-    if ($state == 0) {
-        next unless /^(\s*)\S+\s*:/;
-        my $spaces = $1;
-        my $len = length $spaces;
-        while (scalar @nests && $len < $nests[$#nests]) {
-            pop @nests;
-        }
-        push @nests, $len if (! scalar @nests || $len > $nests[$#nests]);
-        if (/^\s*($key)\s*:\s*([|>][-+]?|\$\{\{.*|(?:"\s*|)$value)\s*$/) {
-            $gh_yaml_mode = $2;
-            ($start_line, $start_pos, $end) = ($., $-[1] + 1, $+[2] + 1);
-            report() if ($gh_yaml_mode =~ /$value|\$\{\{/);
-            $state = 1;
-        }
-    } elsif ($state == 1) {
-        if (/^\s*(?:#.*|)$/) {
-            $end += length $_;
-            continue;
-        }
-        /^(\s*)(\S.*?)\s*$/;
-        my ($spaces, $v) = ($1, $2);
-        $len = length $spaces;
-        if (scalar @nests && $len > $nests[$#nests] && $v =~ /$value/) {
-            $end += $len + length $v;
-            report();
-        }
-        pop @nests;
-        $state = 0;
-    }
-}
+
+use CheckSpelling::Util;
+use CheckSpelling::Yaml;
+
+my $key = CheckSpelling::Util::get_file_from_env('KEY', undef);
+my $value = CheckSpelling::Util::get_file_from_env('VALUE', undef);
+my $message = CheckSpelling::Util::get_file_from_env('MESSAGE', '');
+exit unless $key && $value;
+CheckSpelling::Yaml::check_yaml_key_value(
+  quotemeta($key),
+  quotemeta($value),
+  $message
+);
