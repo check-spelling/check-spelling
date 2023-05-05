a43adea231e8c6e7e9e29a3b84678b2f3d3893b9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-05-04 21:02:27 -0400

    Add CheckSpelling::Yaml::get_yaml_value

--- lib/CheckSpelling/Yaml.pm
+++ lib/CheckSpelling/Yaml.pm
@@ -11,6 +11,91 @@ sub report {
   exit;
 }
 
+sub get_yaml_value {
+  my ($file, $path) = @_;
+  my @path_split = split /\./, $path;
+  my $level = 0;
+  my @prefixes;
+  open $yaml, '<', $file || return '';
+  my @result;
+  my $line_result;
+  my $mode;
+  my $last;
+  while (<$yaml>) {
+    chomp;
+    next if /^\s*#/;
+    if (/^(\s*)(\S.*)/) {
+      my ($prefix, $remainder) = ($1, $2);
+      while ($level && length $prefix < length $prefixes[$level - 1]) {
+        delete $prefixes[$level--];
+      }
+      if (@result && $level < scalar @path_split) {
+        $last = 1;
+        break;
+      }
+      last if $last;
+      if (!$level || length $prefix > length $prefixes[$level - 1]) {
+        if ($level == scalar @path_split) {
+          push @result, $remainder;
+        } else {
+          my $next = $path_split[$level];
+          if ($remainder =~ /$next:(.*)$/) {
+            $prefixes[$level++] = $prefix;
+            if ($level == scalar @path_split) {
+              $mode = $1;
+              if ($mode =~ /\s*([-+>|]+)\s*$/) {
+                $mode = $1;
+              } elsif ($mode =~ /\s*(\S.*?)\s*$/) {
+                my $value = $1;
+                $value =~ s/^'(.*)'$/$1/;
+                $line_result = $value;
+                last;
+              }
+            }
+          }
+        }
+      }
+    } elsif (/^\s*$/ && @result) {
+      push @result, '';
+    }
+  }
+  close $yaml;
+  return $line_result unless @result;
+  $mode =~ /([-+])/;
+  my $newlines = $1;
+  $mode =~ /([|>]?)/;
+  $mode = $1;
+  my $suffix;
+  if ($newlines eq '') {
+    $suffix = "\n";
+  }
+  unless ($newlines eq '+') {
+    pop @result while (@result[$#result] eq '');
+  }
+  if ($mode eq '') {
+    return (join ' ', @result).$suffix;
+  }
+  if ($mode eq '|') {
+    return (join "\n", @result).$suffix;
+  }
+  if ($mode eq '>') {
+    my @output;
+    my $tentative;
+    while (@result) {
+      my $line = shift @result;
+      if ($line eq '') {
+        push @output, $tentative;
+        $tentative = '';
+      } else {
+        $tentative .= $line;
+      }
+    }
+    push @output, $tentative;
+    return (join "\n", @output).$suffix;
+  }
+  return (join ' ? ', @result).$suffix;
+}
+
 sub check_yaml_key_value {
   my ($key, $value, $message) = @_;
   my ($state, $gh_yaml_mode) = (0, '');
