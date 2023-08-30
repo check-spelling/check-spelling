39353d0d4aacb6129ce5ccdf7e74ef9abab0a597
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-08-29 21:49:44 -0400

    fast-install: Improve handling for perl-brew

--- fast-install.pl
+++ fast-install.pl
@@ -5,8 +5,14 @@ use File::Basename;
 use File::Path qw(make_path);
 
 my $dirname = dirname(__FILE__);
+my $dest;
 my @shared=grep { m{share/perl} } @INC;
-my $dest=$shared[0];
+if (@shared) {
+    $dest=$shared[0];
+} else {
+    @shared=grep { !m{thread} } @INC;
+    $dest=$shared[$#shared];
+}
 
 make_path($dest, {
     mode => 0755
