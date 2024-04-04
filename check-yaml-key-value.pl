48d9e89ab26827ccb346f99b241f0fbed81e8c86
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-04-04 14:10:10 -0400

    check-yaml-key-value: Add REPORT_MATCHING_YAML mode
    This enables clients to get the matching key value instead of providing a message

--- check-yaml-key-value.pl
+++ check-yaml-key-value.pl
@@ -6,9 +6,11 @@ use CheckSpelling::Yaml;
 my $key = CheckSpelling::Util::get_file_from_env('KEY', undef);
 my $value = CheckSpelling::Util::get_file_from_env('VALUE', undef);
 my $message = CheckSpelling::Util::get_file_from_env('MESSAGE', '');
+my $report_match = CheckSpelling::Util::get_file_from_env('REPORT_MATCHING_YAML', '');
 exit unless $key && $value;
 CheckSpelling::Yaml::check_yaml_key_value(
   quotemeta($key),
   quotemeta($value),
-  $message
+  $message,
+  $report_match,
 );
