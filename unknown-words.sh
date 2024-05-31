5714601156ddef884c939833a68051b29eeea34c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-05-31 09:25:43 -0400

    Replace Microsoft Outlook hack
    Microsoft Outlook.app doesn't understand:
    <details><summary></summary></details>
    
    ... specifically, it treats them as a span which
    results in some truly lousy rendering.
    
    The previous hack of introducing an emoji at least
    prevented users from seeing a running sentence.
    
    The new hack is to introduce an empty `<p></p>`.
    Web browsers ignore it (because they already have
    a break), but Microsoft Outlook.app treats it as a
    paragraph break or something which is desirable.

--- unknown-words.sh
+++ unknown-words.sh
@@ -2471,7 +2471,7 @@ remove_items() {
     if [ -s "$remove_words" ]; then
       echo "
         <details><summary>These words are not needed and should be removed
-        </summary>$(cat "$remove_words")$N🫥$N</details>
+        </summary>$(cat "$remove_words")$N</details><p></p>
       " | strip_lead_and_blanks
       set_output_variable stale_words "$remove_words"
     else
