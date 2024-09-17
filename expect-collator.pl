a8cbb914ebdf7a072b17c8fa1a5f25c32ae64bf9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-17 09:20:16 -0400

    expect-collator: Expose early warnings
    When there are forbidden patterns in expect.txt, we actually need to expose them...

--- expect-collator.pl
+++ expect-collator.pl
@@ -17,9 +17,15 @@ close EXPECT;
 my $pattern = '\`(?:'.join('|', map { quotemeta($_) } @words).')`';
 open SOURCES, '<', $notes;
 while (<SOURCES>) {
-  next unless $_ =~ /$pattern/;
-  s/not a recognized word/ignored by check spelling because another more general variant is also in expect/;
-  s/unrecognized-spelling/ignored-expect-variant/;
+  if ($_ =~ /$pattern/) {
+    $print = 0;
+    $print = 1 if s/not a recognized word/ignored by check spelling because another more general variant is also in expect/;
+    $print = 1 if s/unrecognized-spelling/ignored-expect-variant/;
+    next unless $print;
+  } else {
+    next unless /\(((?:\w+-)+\w+)\)$/;
+    next if $1 eq 'unrecognized-spelling';
+  }
   print;
 }
 close SOURCES;
