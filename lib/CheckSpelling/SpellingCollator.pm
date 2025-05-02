7f491bbeb1815084b0d69cfbb80df2ff7261309b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-02 01:49:21 -0400

    SpellingCollator: Improve noisy-file message

--- lib/CheckSpelling/SpellingCollator.pm
+++ lib/CheckSpelling/SpellingCollator.pm
@@ -412,9 +412,11 @@ sub main {
         # || ($unrecognized > $words / 2)
     ) {
       unless ($disable_noisy_file) {
-        push @delayed_warnings, "$file:1:1 ... 1, Warning - Skipping `$file` because there seems to be more noise ($unknown) than unique words ($unique) (total: $unrecognized / $words). (noisy-file)\n";
         if ($file ne $file_list) {
+          push @delayed_warnings, "$file:1:1 ... 1, Warning - Skipping `$file` because it seems to have more noise ($unknown) than unique words ($unique) (total: $unrecognized / $words). (noisy-file)\n";
           print SHOULD_EXCLUDE "$file\n";
+        } else {
+          push @delayed_warnings, "$file:1:1 ... 1, Warning - Skipping file list because there seems to be more noise ($unknown) than unique words ($unique) (total: $unrecognized / $words). (noisy-file-list)\n";
         }
         push @directories, $directory;
         next;
