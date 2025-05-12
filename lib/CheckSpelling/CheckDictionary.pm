5b54b9ba2dcef199a74d372ff4ca5c00f1edf350
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-11 21:20:40 -0400

    CheckDictionary: Tolerate empty INPUT_IGNORE_PATTERN

--- lib/CheckSpelling/CheckDictionary.pm
+++ lib/CheckSpelling/CheckDictionary.pm
@@ -5,7 +5,8 @@ package CheckSpelling::CheckDictionary;
 sub process_line {
     my ($file, $line) = @_;
     $line =~ s/$ENV{comment_char}.*//;
-    if ($line =~ /^.*?($ENV{INPUT_IGNORE_PATTERN}+)/) {
+    my $ignore_pattern = $ENV{INPUT_IGNORE_PATTERN};
+    if ($ignore_pattern ne '' && $line =~ /^.*?($ignore_pattern+)/) {
         my ($left, $right) = ($-[1] + 1, $+[1] + 1);
         my $column_range="$left ... $right";
         return ('', "$file:$.:$column_range, Warning - Ignoring entry because it contains non-alpha characters. (non-alpha-in-dictionary)\n");
